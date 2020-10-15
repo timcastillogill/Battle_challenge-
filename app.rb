67d9e34f9cf0974035e35c73249d9e62d32915b4
Cameron Logie <cameronlogie24@gmail.com>
2020-10-15 17:19:38 +0100

    added hp for both players, visible on /play

--- app.rb
+++ app.rb
@@ -9,15 +9,12 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    # "player_1 = " << session[:player_1].inspect
-    # "player_2 = " << session[:player_2].inspect
     session[:player_1] = params[:player_1]
     session[:player_2] = params[:player_2]
     redirect to('/play')
   end
 
   get '/play' do
-
     @player_1 = session[:player_1]
     @player_2 = session[:player_2]
     erb :play
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,13 +1,19 @@
 # require 'app.rb'
 
 feature 'Names form' do
-  scenario 'can take in two player names from a form and post them to /names' do
+  scenario 'can take in two player names from a form and post them to /names which redirects to /play and are visible there' do
     visit('/')
     fill_in(:player_1, with: 'Tim')
     fill_in(:player_2, with: 'Cam')
     click_button('Submit')
     expect(page).to have_content('Tim' && 'Cam')
   end
+end
+feature 'view hitpoints' do
+  scenario 'you can see the hitpoints that each player still has' do
+    visit('/play')
+    expect(page).to have_content( 100 && 100 )
+end
 end
 
 
